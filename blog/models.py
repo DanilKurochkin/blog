549fb370f9413a9692a5794a24df0a38c0cd70ae
DanilKurochkin <78516434+DanilKurochkin@users.noreply.github.com>
2023-07-13 18:26:50 +0300

    added posts

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=60)
+    description = models.CharField(max_length=120)
+    date = models.DateField()
